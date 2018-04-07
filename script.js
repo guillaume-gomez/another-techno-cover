06b6d9828a406a22e5b8bbf34fe4384f6460fe81
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-04-07 22:55:52 +0200

    add index value

--- script.js
+++ script.js
@@ -8,26 +8,27 @@ var fileUpload6 = document.getElementById('fileUpload6');
 var canvas  = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
 
-function readImage() {
-  if (this.files && this.files[0]) {
-      var FR= new FileReader();
+function readImage(event, index) {
+  if (event.target.files && event.target.files[0]) {
+      var FR = new FileReader();
       FR.onload = function(e) {
          var img = new Image();
          img.src = e.target.result;
          img.onload = function() {
            ctx.drawImage(img, 0, 0, 512, 512);
+           ctx.clearRect(30,50, Math.random() * 100, Math.random() * 400);
          };
       };
-      FR.readAsDataURL(this.files[0]);
+      FR.readAsDataURL(event.target.files[0]);
   }
 }
 
-fileUpload1.onchange = readImage;
-fileUpload2.onchange = readImage;
-fileUpload3.onchange = readImage;
-fileUpload4.onchange = readImage;
-fileUpload5.onchange = readImage;
-fileUpload6.onchange = readImage;
+fileUpload1.onchange = (event) => { readImage(event, 1); };
+fileUpload2.onchange = (event) => { readImage(event, 2); };
+fileUpload3.onchange = (event) => { readImage(event, 3); };
+fileUpload4.onchange = (event) => { readImage(event, 4); };
+fileUpload5.onchange = (event) => { readImage(event, 5); };
+fileUpload6.onchange = (event) => { readImage(event, 6); };
 
 canvas.onclick = function(e) {
   var x = e.offsetX;
