eb7c7b78a5eccd5047e42f6eaf629d42f138157b
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-04-07 23:08:15 +0200

    make the algorithm functionnal

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var fileUpload6 = document.getElementById('fileUpload6');
 
 var canvas  = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
+ctx.globalCompositeOperation="destination-over";
+
 
 function readImage(event, index) {
   if (event.target.files && event.target.files[0]) {
@@ -15,8 +17,8 @@ function readImage(event, index) {
          var img = new Image();
          img.src = e.target.result;
          img.onload = function() {
-           ctx.drawImage(img, 0, 0, 512, 512);
            ctx.clearRect(30,50, Math.random() * 100, Math.random() * 400);
+           ctx.drawImage(img, 0, 0, 512, 512);
          };
       };
       FR.readAsDataURL(event.target.files[0]);
@@ -29,12 +31,3 @@ fileUpload3.onchange = (event) => { readImage(event, 3); };
 fileUpload4.onchange = (event) => { readImage(event, 4); };
 fileUpload5.onchange = (event) => { readImage(event, 5); };
 fileUpload6.onchange = (event) => { readImage(event, 6); };
-
-canvas.onclick = function(e) {
-  var x = e.offsetX;
-  var y = e.offsetY;
-  ctx.beginPath();
-  ctx.fillStyle = 'black';
-  ctx.arc(x, y, 5, 0, Math.PI * 2);
-  ctx.fill();
-};
